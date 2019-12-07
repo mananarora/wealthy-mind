d154b44e449b3b8486ed8740bc8369d4ffbf009c
Manan <manan.arora@cerner.com>
2019-12-07 03:35:44 -0600

    header setup

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../stylesheets/styles.css'
+
+const Header = () => {
+    return (
+        <div className="App-header">
+            <h1>WealthyMind</h1>
+        </div>
+    );
+};
+
+export default Header;
